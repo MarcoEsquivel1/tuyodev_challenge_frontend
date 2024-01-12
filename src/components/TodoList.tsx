0dfeb2bc2aab5b5d7c82a4c3298868dcddbadcdb
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2024-01-11 21:54:07 -0600

    feat(styles): Enter and Exit animations in TodoList
    Use AnimatedPResence to add animations to the list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,11 +4,27 @@ import { useModal } from "../hooks/useModal"
 import { RootState } from "../stores/taskStore"
 import { useSelector } from "react-redux"
 import { TodoItem } from "./TodoItem"
+import { AnimatePresence, motion } from "framer-motion"
+
+const animatedItem = {
+    hidden: { opacity: 0, x: 50 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        x: 0,
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+    exit: {
+        scaleY: 0, opacity: 0, zIndex: -1,
+    },
+};
 
 export const TodoList = () => {
-    const { showModal, toggleModal } = useModal()
+    const { showModal, toggleModal } = useModal();
 
-    const tasks = useSelector((state: RootState) => state.tasks.tasks)
+    const tasks = useSelector((state: RootState) => state.tasks.tasks);
 
     return (
         <>
@@ -17,19 +33,31 @@ export const TodoList = () => {
             </Row>
             <Row>
                 <Col>
-                    <Button variant="primary" onClick={toggleModal}>Add Task</Button>
+                    <Button variant="primary" onClick={toggleModal}>
+                        Add Task
+                    </Button>
                 </Col>
-                <Col>
-                </Col>
+                <Col></Col>
             </Row>
-                
-            {tasks.map(task => (
-            <Row>
-                <TodoItem key={task.id} task={task}/>
-            </Row>
-            ))}
-                
-            <TodoForm showModal={showModal} toggleModal={toggleModal}/>
+            <AnimatePresence>
+                {tasks.map((task) => (
+                    <motion.div
+                        key={task.id}
+                        variants={animatedItem}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                    >
+                        <Row>
+                            <motion.div variants={animatedItem}>
+                                <TodoItem task={task} />
+                            </motion.div>
+                        </Row>
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+
+            <TodoForm showModal={showModal} toggleModal={toggleModal} />
         </>
-    )
-}
+    );
+};
